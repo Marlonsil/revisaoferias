f01c69f8f4e6cad229dfda19a70767a550332339
Marlonsil <marlonsilveira397@gmail.com>
2024-07-31 17:50:53 +0000

    salva

---
+++ revisao/app/(tabs)/home.tsx
@@ -0,0 +1,33 @@
+import { View, Image, StyleSheet,Text,Button } from 'react-native';
+import { useRouter } from 'expo-router';
+
+
+export default function Home() {
+const router = useRouter();
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Home Screen</Text>
+        <Button title ="Ir para Página" onPress={() => router.push('./about')} color={"#1F511E"}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor:'#000',
+
+    },
+    title:{
+        fontSize: 28,
+        color:'#86FF84',
+        marginBottom:20,
+        fontFamily:'Inter'
+    }
+    
+
+
+});
